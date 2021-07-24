7df46fe5719ebf8b1fdbcdf3a3cd2ee32fcfe1e8
RamDurgaSai <ramdurgasais@gmail.com>
2021-07-24 12:25:33 +0530

    Initial v1.0

---
+++ main.py
@@ -0,0 +1,42 @@
+import socket
+import json
+import sys
+class Server:
+    def __init__(self):
+        self.host_address = socket.gethostbyname(socket.gethostname())
+        self.load_settings()
+        self.say_welcome()
+
+        pass
+    def run(self):
+        pass
+
+    def load_settings(self):
+        with open("settings.json", "r", encoding="utf-8") as config_file:
+            settings = json.load(config_file)
+
+        if settings["__version__"]:
+            self._version = settings["__version__"]
+        if settings["LICENCE"]:
+            self._licence = settings["LICENCE"]
+        if settings["URL"]:
+            self._url = settings["URL"]
+        if settings["PORT"]:
+            self._port = settings["PORT"]
+
+    def say_welcome(self):
+        welcome_string = "".join((
+            "CNC Messaging Serivce Server ",
+            "v",str(self._version)," ",
+            "Copyright (C) 2021 ",str(self._url),"\n"
+            "Licensed under the terms of the MIT License ","\n",
+            "Running on IP : "+str(self.host_address),
+            " Port : " + str(self._port), "\n"
+        ))
+        sys.stdout.write(welcome_string)
+
+
+
+
+if __name__ == '__main__':
+    Server().run()
